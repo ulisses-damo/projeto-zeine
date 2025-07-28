0abb225eb5294b88d8568cbe310fa6e50f2b12e1
ulisses-damo <ulissesdamo@gmail.com>
2025-07-28 10:39:05 -0300

    Erro de deploy corrigido

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../styles/Home.css";
+import "../styles/home.css";
 
 export default function Home() {
   const [contacts, setContacts] = useState([
